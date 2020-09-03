b7ee67b6e51c44202e7a942d50018d9bff761598
Filip Cordas <filipcordas@Solan-Gundersens-2013-MacBook-Pro.local>
2020-09-03 15:01:41 +0200

    window is not defined fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { Link } from 'gatsby';
 import HeadHelmet from '../components/Head';
-import ReactTypingEffect from '../vendor/react-typing-effect';
+import Typing from 'react-typing-animation';
 
 import styles from './index.module.scss';
 
 const Home = () => (
   <div className={styles.wrapper}>
     <HeadHelmet title="Home" />
-    <ReactTypingEffect
-      text={['HI. I AM FILIP.', 'WELCOME :-)']}
-      speed={80}
-      typingDelay={500}
-      eraseDelay={300}
+    <Typing
+      speed={70}
+      startDelay={500}
       className={styles.header}
-    />
+      loop={true}
+      cursorClassName={styles.cursor}
+    >
+      <div>
+      HI.
+      <Typing.Delay ms={300} />
+      <nbsp> </nbsp>I AM FILIP.
+      <Typing.Delay ms={500} />
+      <Typing.Backspace count={16} />
+      <Typing.Delay ms={300} />
+      WELCOME :-)
+      <Typing.Delay ms={1000} />
+      <Typing.Backspace count={16} />
+      </div>
+    </Typing>
     <nav>
       <ul className={styles.navList}>
         <li>
